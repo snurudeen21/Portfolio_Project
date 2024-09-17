98d3ac10e29849db4b1aec9f27333645454250c5
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-09-17 15:00:34 +0100

    Initializing my App

---
+++ app/__init__.py
@@ -0,0 +1,32 @@
+# app/__init__.py
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_migrate import Migrate
+
+
+# Initialize extensions
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+migrate = Migrate()
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'your_secret_key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+    # Initialize extensions with app
+    db.init_app(app)
+    bcrypt.init_app(app)
+    migrate.init_app(app, db)
+    login_manager.init_app(app)
+
+    # Import and register blueprints
+    from app.routes import main
+    app.register_blueprint(main)
+
+    return app
